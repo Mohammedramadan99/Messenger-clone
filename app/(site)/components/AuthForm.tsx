3b7eb6c5c3af4b72c1ef1bfbdf95b6ae53c26020
mohammedramadan99 <lifeisagame0101@gmail.com>
2023-08-18 00:29:30 +0300

    db: connect prisma to mongodb

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,10 @@
 "use client";
+import Button from "@/app/components/Button";
 import Input from "@/app/components/Input/Input";
 import { useState, useCallback } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 // TYPES
 type variant = "LOGIN" | "REGESTER";
@@ -39,11 +42,77 @@ export default function AuthForm() {
   const socialAction = (action: String) => {
     setIsLoading(true);
   };
-  return <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-md">
-    <div className="bg-white px-4 py-8 sm:rounded-lg sm:px-10">
-      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-        <Input/>
-      </form>
+  return (
+    <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGESTER" && (
+            <Input
+              id="name"
+              lable="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            lable="Email"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            lable="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit" secondary>
+              {variant === "LOGIN" ? "Sign-In" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex gap-2">
+          <AuthSocialButton
+            icon={BsGithub}
+            onClick={() => socialAction("github")}
+          />
+
+          <AuthSocialButton
+            icon={BsGoogle}
+            onClick={() => socialAction("google")}
+          />
+        </div>
+      </div>
+      <div className="mt-6 flex gap-2 justify-center text-sm px-2 text-gray-500">
+        <div>
+          {variant === "LOGIN"
+            ? "New to Messenger?"
+            : "Already have an account"}
+        </div>
+        <div onClick={toggleVariant} className="underline cursor-pointer">
+          {variant === "LOGIN" ? "Create an account" : "Login"}
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 interface InputProps {
-  id: String;
+  id: string;
   lable: string;
   type?: string;
   required?: boolean;
@@ -10,6 +10,38 @@ interface InputProps {
   disabled?: boolean;
 }
 
-export default function Input() {
-  return <div>Input</div>;
-}
+const Input: React.FC<InputProps> = ({
+  id,
+  lable,
+  type,
+  required,
+  errors,
+  disabled,
+  register,
+}) => {
+  return (
+    <div>
+      <label
+        className="block text-sm font-medium leading-6 text-grey-900"
+        htmlFor={id}>
+        {lable}
+      </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
